0a5ed49ab1c27d8b79c12d1bed4060b9eefac569
khaya05 <khayamnyandu5@gmail.com>
2023-05-24 14:19:22 +0200

    fixed destructuring bugs

--- 01-task-manager/app.js
+++ 01-task-manager/app.js
@@ -6,6 +6,7 @@ const taskRouter = require('./routes/tasksRouter');
 const app = express();
 
 // middleware
+app.use(express.static('./public'));
 app.use(morgan('dev'));
 app.use(express.json());
 
