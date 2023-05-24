7273d565a75a3001956021d3c7a9c32a7903b5ba
khaya05 <khayamnyandu5@gmail.com>
2023-05-24 11:10:51 +0200

    DB connected

--- 01-task-manager/server.js
+++ 01-task-manager/server.js
@@ -1,5 +1,23 @@
+const mongoose = require('mongoose');
 const app = require('./app');
+const dotenv = require('dotenv');
 
-const port = 3000;
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((con) => console.log('DB connection successful'));
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`app running on port: ${port}...`));
