e3af6d721178106c18e9cc246eecb47b500cd2ca
khaya05 <khayamnyandu5@gmail.com>
2023-05-24 10:32:04 +0200

    used morgan

--- 01-task-manager/app.js
+++ 01-task-manager/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const taskRouter = require('./routes/tasksRouter');
 
 const app = express();
 
 // middleware
-app.use(express.json())
+app.use(morgan('dev'));
+app.use(express.json());
 
 // router
 app.use('/api/v1/tasks', taskRouter);
