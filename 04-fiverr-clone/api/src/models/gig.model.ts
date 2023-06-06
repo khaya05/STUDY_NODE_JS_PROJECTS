3b602432551968e873be4ea4eee9f8854cbee167
khaya05 <khayamnyandu5@gmail.com>
2023-06-06 11:51:26 +0200

    gig model

--- 04-fiverr-clone/api/src/models/gig.model.ts
+++ 04-fiverr-clone/api/src/models/gig.model.ts
@@ -1,7 +1,68 @@
 import mongoose from 'mongoose';
 
 const gigSchema = new mongoose.Schema(
-  {},
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    dec: {
+      type: String,
+      required: true,
+    },
+    totalStarts: {
+      type: Number,
+      default: 0,
+    },
+    starNumber: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    cover: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+      required: false,
+    },
+    shortTitle: {
+      type: String,
+      required: true,
+    },
+    shortDesc: {
+      type: String,
+      required: false,
+    },
+    deliveryTime: {
+      type: Number,
+      default: 0,
+    },
+    sales: {
+      type: Number,
+      default: 0,
+    },
+    revisionNumber: {
+      type: Number,
+      default: 0,
+    },
+    features: {
+      type: [String],
+      required: false,
+    },
+  },
   {
     timestamps: true,
   }
